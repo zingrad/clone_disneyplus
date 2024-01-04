66a26f4b1e1d35ed5889bf15eaccd8f766e6cd1f
zingrad <erick.isabelle@gmail.com>
2024-01-04 20:21:01 -0300

    Finalizando FAQ

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const buttons = document.querySelectorAll('[data-tab-button]');
+    const buttons = document.querySelectorAll('[data-tab-button]'); //pane
+    const questions = document.querySelectorAll('[data-faq-question]'); //accordion
     
-
+ //logica para tab pane
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,8 +13,24 @@ document.addEventListener("DOMContentLoaded", function(){
            botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    //logica para accordion
+    for (let i = 0; i <questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
+//accordion lógica
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+   const elementoPai = elemento.target.parentNode;
+  
+   elementoPai.classList.toggle(classe);
+}
+
+
+
+//pane lógica
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
 
